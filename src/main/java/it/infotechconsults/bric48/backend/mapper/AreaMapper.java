d3bfd6daad6347a39421a3d93e4ce343feb0c79e
Nico Cardone <nico.cardone@sincon.it>
2024-11-26 19:27:15 +0000

    [WIP] fixed mapper

--- src/main/java/it/infotechconsults/bric48/backend/mapper/AreaMapper.java
+++ src/main/java/it/infotechconsults/bric48/backend/mapper/AreaMapper.java
@@ -1,13 +1,30 @@
 package it.infotechconsults.bric48.backend.mapper;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import it.infotechconsults.bric48.backend.domain.Area;
+import it.infotechconsults.bric48.backend.repository.StructureRepository;
 import it.infotechconsults.bric48.backend.rest.dto.AreaDTO;
 import it.infotechconsults.bric48.backend.rest.dto.AreaResponseDTO;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public abstract class AreaMapper extends BaseMapper<AreaDTO, Area, AreaResponseDTO> {
 
+    @Autowired
+    protected StructureRepository structureRepository;
+    
+    @Override
+    @Mapping(target = "structure", expression = "java(structureRepository.findById(request.getStructureId()).orElse(null))")
+    public abstract Area requestToEntity(AreaDTO request);
+
+    @Override
+    @BeanMapping(nullValuePropertyMappingStrategy =  NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "structure", expression = "java(structureRepository.findById(request.getStructureId()).orElse(null))")
+    public abstract void updateEntity(@MappingTarget Area entity, AreaDTO request);
 }
